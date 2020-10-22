0da06d1415757ee02a6a225f525a995a02e3db6e
Russell Cohen <rcoh@amazon.com>
2020-10-22 16:08:14 -0400

    Update copyright notices again

--- test.sh
+++ test.sh
@@ -1,3 +1,8 @@
+#
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0.
+#
+
 set -e
 declare REPO_ROOT
 REPO_ROOT="$(git rev-parse --show-toplevel)"
